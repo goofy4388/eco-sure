08969d70ce160747178165cb9fe9041b7a90183f
goofy4388 <5jwwydzdtz@privaterelay.appleid.com>
2025-12-29 11:21:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 import express from "express";
-
 const app = express();
 
-// Serve all files in this folder (index.html, styles.css, app.js)
 app.use(express.static("."));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`BBI Checklist running at: http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`));
